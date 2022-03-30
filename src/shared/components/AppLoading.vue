15518b18f675f9d6cd2731ded0c4e596f03eb363
Jeyson Calvache <jeison130@gmail.com>
2022-03-30 17:17:55 -0500

    Loading Component

---
+++ src/shared/components/AppLoading.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="d-flex justify-content-center">
+    <div class="spinner-border text-primary" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'AppLoading',
+});
+</script>
+
+<style scoped>
+
+</style>
