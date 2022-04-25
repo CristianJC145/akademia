58e100c539c922d0491e561cc212a69d5b36b715
Jeyson Calvache <jeison130@gmail.com>
2022-04-24 23:35:14 -0500

    Mis compras con paginacion

--- src/modules/casurid/pages/Shopping.vue
+++ src/modules/casurid/pages/Shopping.vue
@@ -13,18 +13,50 @@
 
             <select class="form-select">
               <option value="" selected disabled>Nivel</option>
+              <option v-for="level in levels.value" :value="level.id">
+                {{ level.name }}
+              </option>
             </select>
 
             <select class="form-select">
               <option value="" selected disabled>Grado</option>
+              <option v-for="degree in degrees.value" :value="degree.id">
+                {{ degree.name }}
+              </option>
             </select>
 
             <select class="form-select">
               <option value="" selected disabled>Asignatura</option>
+              <option v-for="subject in subjects.value" :value="subject.id">
+                {{ subject.name }}
+              </option>
             </select>
           </div>
-          <div>
-            Tabla
+          <div class="mt-4">
+            <AppDatatable :service="getShoppingService">
+              <template v-slot:head>
+                <tr>
+                  <th scope="col">Plan</th>
+                  <th scope="col">Nivel</th>
+                  <th scope="col">Grado</th>
+                  <th scope="col">Asignatura</th>
+                  <th scope="col">Cantidad</th>
+                  <th scope="col">Vigencia</th>
+                </tr>
+              </template>
+
+              <template v-slot:body="{ data }">
+                <tr v-for="shop in data">
+                  <th>{{ shop.productName }}</th>
+                  <td>{{ shop.levelName }}</td>
+                  <td>{{ shop.DegreeName }}</td>
+                  <td>{{ shop.subjectName }}</td>
+                  <td>{{ shop.quantity }}</td>
+                  <td>{{ shop.validUntil }}</td>
+                </tr>
+              </template>
+            </AppDatatable>
+
           </div>
         </div>
       </div>
@@ -33,13 +65,24 @@
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+import {defineComponent, onMounted, reactive} from 'vue';
 import AppBaseList from '../../../shared/components/AppBaseList.vue';
+import AppPagination from '../../../shared/components/AppPagination.vue';
 import {BreadCrumbsType} from '../../../shared/types/breadCrumbs.type';
+import {GetFiltersShoppingService} from '../services/getFiltersShopping.service';
+import {GetShoppingService} from '../services/getShopping.service';
+import {LevelDto} from '../dtos/level.dto';
+import {DegreeDto} from '../dtos/degree.dto';
+import {SubjectDto} from '../dtos/subject.dto';
+import {ShoppingDto} from '../dtos/shopping.dto';
+import AppDatatable from '../../../shared/components/AppDatatable.vue';
+
+const getFiltersShoppingService = new GetFiltersShoppingService();
+
 
 export default defineComponent({
   name: 'Shopping',
-  components: {AppBaseList},
+  components: {AppDatatable, AppPagination, AppBaseList},
   setup() {
     const title = 'Mis Compras';
     const routes: BreadCrumbsType[] = [
@@ -52,9 +95,40 @@ export default defineComponent({
       },
     ];
 
+    const levels: { value: LevelDto[] } = reactive({
+      value: [],
+    });
+
+    const degrees: { value: DegreeDto[] } = reactive({
+      value: [],
+    });
+
+    const subjects: { value: SubjectDto[] } = reactive({
+      value: [],
+    });
+
+    const shopping: { value: ShoppingDto[] } = reactive({
+      value: [],
+    });
+
+    onMounted(async () => {
+      const filters = await getFiltersShoppingService.run();
+
+      levels.value = filters.levels;
+      degrees.value = filters.degrees;
+      subjects.value = filters.subjects;
+    });
+
+    const getShoppingService = new GetShoppingService();
+
     return {
       title,
       routes,
+      levels,
+      degrees,
+      subjects,
+      shopping,
+      getShoppingService,
     };
   },
 });
