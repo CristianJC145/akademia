84292a0a1321887b0f6e327617064ecea5ca0de4
Jeyson Calvache <jeison130@gmail.com>
2022-04-25 16:30:57 -0500

    Button Loading

---
+++ src/shared/components/AppButtonLoading.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="btn d-flex align-items-center gap-2" :disabled="loading">
+    <div class="spinner-border" role="status" v-if="loading">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref, watch} from 'vue';
+
+export default defineComponent({
+  name: 'AppButtonLoading',
+  props: ['loading'],
+  setup(props) {
+    const loading = ref(props.loading);
+
+    watch(() => props.loading, (value) => {
+      loading.value = value;
+    });
+
+    return {
+      loading,
+    };
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
