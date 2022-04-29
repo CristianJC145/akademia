8db4b84f99151445cdb7b2750e31b29044e83e00
Jeyson Calvache <jeison130@gmail.com>
2022-04-29 18:00:05 -0500

    Filtros listado de contenidos

--- src/modules/casurid/pages/Contents.vue
+++ src/modules/casurid/pages/Contents.vue
@@ -1,16 +1,28 @@
 <template>
   <AppBaseList title="Contenidos">
-  aqui
+    aqui
   </AppBaseList>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+import {defineComponent, onMounted} from 'vue';
 import AppBaseList from '../../../shared/components/AppBaseList.vue';
+import {GetFiltersContentService} from '../services/getFiltersContent.service';
+
+const getFiltersContentService = new GetFiltersContentService();
 
 export default defineComponent({
   name: 'Contents',
   components: {AppBaseList},
+  setup() {
+    onMounted(async () => {
+      const response = await getFiltersContentService.run();
+
+      console.log(response);
+    });
+
+    return {};
+  },
 });
 </script>
 
