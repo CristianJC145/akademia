cf9ab9b0b8d2f6501ecc6e1e111dc6ac47203a58
Jeyson Calvache <jeison130@gmail.com>
2022-04-29 12:01:51 -0500

    Moviendo login

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import {createApp} from 'vue';
-import Amplify from 'aws-amplify';
-import {translations} from '@aws-amplify/ui';
-import {I18n} from 'aws-amplify';
 
 import App from './App.vue';
 import plugins from './shared/plugins';
 import {router} from './app.routing';
-import {amplifyConstant} from './shared/constant/amplify.constant';
 import VueToast from 'vue-toast-notification';
 import i18n from './shared/plugins/i18n.plugin';
 // @ts-ignore
@@ -18,10 +14,6 @@ import './assets/theme/index.scss';
 import 'vue-select/dist/vue-select.css';
 import 'bootstrap';
 import {TooltipDirective} from './shared/directives/tooltip.directive';
-
-Amplify.configure(amplifyConstant);
-I18n.putVocabularies(translations);
-I18n.setLanguage('es');
 
 const app = createApp(App);
 
