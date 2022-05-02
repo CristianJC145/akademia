2eab4060263b00985da709295b598f24633803a9
Jeyson Calvache <jeison130@gmail.com>
2022-05-02 11:22:59 -0500

    Componente boton / enlace de edición

---
+++ src/shared/components/AppButtonEdit.vue
@@ -0,0 +1,30 @@
+<template>
+  <router-link v-if="to" :to="to"
+               class="btn btn-outline-primary">
+    <AppIcon icon="pencil-alt"></AppIcon>
+  </router-link>
+
+  <button v-else class="btn btn-outline-primary">
+    <AppIcon icon="pencil-alt"></AppIcon>
+  </button>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import AppIcon from './AppIcon.vue';
+
+export default defineComponent({
+  name: 'AppButtonEdit',
+  components: {AppIcon},
+  props: ['to'],
+  setup(props) {
+    return {
+      to: props.to,
+    };
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
