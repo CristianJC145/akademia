173a062c9d669642c6b21123a45f1bd97cfa7b91
Jeyson Calvache <jeison130@gmail.com>
2022-05-03 15:17:34 -0500

    Componenete de Accorddion

---
+++ src/shared/components/Accordion/AppAccordion.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="accordion" :id="id">
+    <slot :accordionId="id"></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import {GenerateIdForComponentsService} from '../../services/generateIdForComponents.service';
+
+const generateIdForComponentsService = new GenerateIdForComponentsService();
+
+export default defineComponent({
+  name: 'AppAccordion',
+  setup() {
+    const id = generateIdForComponentsService.run('accordion');
+
+    return {
+      id,
+    };
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/shared/components/Accordion/AppAccordionItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+              :data-bs-target="`#${id}`"
+              aria-expanded="true" aria-controls="collapseOne">
+        <slot name="head"></slot>
+      </button>
+    </h2>
+    <div :id="id" class="accordion-collapse collapse" :data-bs-parent="`#${accordionId}`">
+      <div class="accordion-body">
+        <slot name="content"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import {GenerateIdForComponentsService} from '../../services/generateIdForComponents.service';
+
+const generateIdForComponentsService = new GenerateIdForComponentsService();
+
+export default defineComponent({
+  name: 'AppAccordionItem',
+  props: ['accordionId'],
+  setup() {
+    const id = generateIdForComponentsService.run('accordion_item');
+
+    return {
+      id,
+    };
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
