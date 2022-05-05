1d6b4e31ab9278c62e86ea087d5d46f63fbbad8b
Jeyson Calvache <jeison130@gmail.com>
2022-05-05 16:44:08 -0500

    Componente de Upload Image

---
+++ src/shared/components/AppUploadImage.vue
@@ -0,0 +1,39 @@
+<template>
+  <input type="file" class="form-control" @change="changeFile"
+         id="file"/>
+  <a :href="currentThumbnail" target="_blank" v-if="currentThumbnail">
+    <small>Ver archivo actual</small>
+  </a>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'AppUploadImage',
+  props: ['currentThumbnail', 'modelValue'],
+  emits: ['update:modelValue'],
+  setup(props, {emit}) {
+
+    const changeFile = (event: any) => {
+      const files = event.target.files;
+
+      if (files[0]) {
+        // form.thumbnail = files[0];
+        emit('update:modelValue', files[0]);
+      } else {
+        emit('update:modelValue', null);
+      }
+    };
+
+    return {
+      changeFile,
+    };
+
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
