2b880a95a9ca47445037f57ac6aa8d61154f075e
Camilo Rodriguez <andrescamilocr482@gmail.com>
2022-05-07 12:47:07 -0500

    Corrección abreviatura en formulario area

--- src/modules/academicPrograms/components/AreaForm.vue
+++ src/modules/academicPrograms/components/AreaForm.vue
@@ -2,22 +2,22 @@
   <form @submit.prevent="save">
     <AppFormModal :title="title">
       <template v-slot:content>
-        <AppFormField :formControl="v$.form.name">
+        <AppFormField :form-control="v$.form.name">
           <label for="name">Nombre</label>
-          <input type="text" class="form-control" required name="name" id="name" v-model="v$.form.name.$model">
+          <input class="form-control" type="text" id="name" v-model="v$.form.name.$model">
         </AppFormField>
-        <AppFormField :formControl="v$.form.abreviation">
-          <label for="abbreviation">Abreviatura</label>
-          <input type="text" class="form-control" name="abbreviation" id="abbreviation"
-                 v-model="v$.form.abbreviation.$model">
+
+        <AppFormField :form-control="v$.form.abbreviation">
+          <label for="abbreviation">Abreviación</label>
+          <input class="form-control" type="text" id="abbreviation" v-model="v$.form.abbreviation.$model">
         </AppFormField>
 
         <AppFormField :form-control="v$.form.file">
           <label for="file">Imagen</label>
-          <AppUploadImage input-id="file" v-model="v$.form.file.$model"
-                          :current-thumbnail="currentThumbnail"></AppUploadImage>
+          <AppUploadImage input-id="file" v-model="v$.form.file.$model" :current-thumbnail="currentThumbnail"></AppUploadImage>
         </AppFormField>
       </template>
+
       <template v-slot:actions>
         <AppButtonLoading type="submit" class="btn-primary text-white">
           Guardar
@@ -33,7 +33,6 @@ import AppFormModal from '../../../shared/components/AppFormModal.vue';
 import AppFormField from '../../../shared/components/AppFormField.vue';
 import AppButtonLoading from '../../../shared/components/AppButtonLoading.vue';
 import {CreateOrUpdateAreaService} from '../services/createOrUpdateArea.service';
-
 import {useVuelidate} from '@vuelidate/core';
 import {required} from '@vuelidate/validators';
 import AppUploadImage from '../../../shared/components/AppUploadImage.vue';
@@ -46,19 +45,21 @@ export default defineComponent({
   emits: ['close'],
   setup(props, {emit}) {
     const data = props.data ?? {};
+    
     let title = 'Nueva área';
 
     if (props.data?.id) {
       title = 'Editar área';
     }
-
+    
     const currentThumbnail = data?.thumbnail;
 
     const form = reactive({
       name: data?.name ?? null,
-      abreviation: data?.abreviation ?? null,
+      abbreviation: data?.abbreviation ?? null,
       file: null,
     });
+    
     const v$ = useVuelidate({
       form: {
         name: {required},
@@ -74,7 +75,6 @@ export default defineComponent({
         await createOrUpdateAreaService.run(form, data?.id);
         emit('close');
       } catch (e) {
-        console.log(e);
       }
     };
     return {
